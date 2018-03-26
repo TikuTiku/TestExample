cfbcfa3dfdafd9e646128c1102cdc38184a6396c
Subhash <Subhash@192.168.0.104>
2018-03-26 23:24:02 +0530

    initial changes

---
+++ src/com/first/repository/ChildClassEx.java
@@ -0,0 +1,9 @@
+package com.first.repository;
+
+public class ChildClassEx 
+{
+	public void TestExample1()
+	{
+		System.out.println("How to Create Child Class Example");
+	}
+}
---
+++ src/com/first/repository/ParentClassEx.java
@@ -0,0 +1,17 @@
+package com.first.repository;
+
+public class ParentClassEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChildClassEx ex=new ChildClassEx();
+		ex.TestExample1();
+		TestExample2();
+
+	}
+	public static void TestExample2()
+	{
+		System.out.println("How to create parent class");
+	}
+
+}
